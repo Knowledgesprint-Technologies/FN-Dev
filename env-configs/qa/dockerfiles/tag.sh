c25fdc81ac50e5e207644f249c62796873a98bde
mohammedasifas <125273867+mohammedasifas@users.noreply.github.com>
2023-04-04 17:47:58 +0530

    Adding taging.txt file to tmp directory

--- env-configs/qa/dockerfiles/tag.sh
+++ env-configs/qa/dockerfiles/tag.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker build -t projectfndev/fn-qa-docker-images:$(docker images | awk '($1 == "projectfndev/fn-qa-docker-images")' | awk 'END {print $2 += .1; exit}') . >> taging.txt
+docker build -t projectfndev/fn-qa-docker-images:$(docker images | awk '($1 == "projectfndev/fn-qa-docker-images")' | awk 'END {print $2 += .1; exit}') . >> /tmp/taging.txt
 
-last_word=$(tail -n 1 taging.txt | awk '{print $NF}')
+last_word=$(tail -n 1 /tmp/taging.txt | awk '{print $NF}')
 
 echo "$last_word"
 docker push $last_word
