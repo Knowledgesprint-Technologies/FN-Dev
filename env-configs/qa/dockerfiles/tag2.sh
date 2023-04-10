7064bf40bd18b8904c7449fac3368e3b78606623
noorunnisamb <noorunnisa.mb@knowledgesprint.com>
2023-04-10 09:41:17 +0000

    updated tag2.sh

--- env-configs/qa/dockerfiles/tag2.sh
+++ env-configs/qa/dockerfiles/tag2.sh
@@ -12,3 +12,4 @@ docker push $Repository/$Imagename:$newtag
 docker images | awk '( $1 == "projectfndev/fn-qa-docker-images")' | awk '{print $2}'| sort -n > /tmp/tagging.txt
 echo $(tail -n 1 /tmp/tagging.txt) > /tmp/tagging.txt
 docker run -dt -p :80 $Repository/$Imagename:$newtag
+echo $(docker ps)
